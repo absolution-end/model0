5232f630d29559358d08e4d343ea28b8ab59bd1c
chirayu Sharma <111888038+absolution-end@users.noreply.github.com>
2024-08-26 17:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
 import pandas as pd
 import os
@@ -12,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+st.set_page_config(layout="wide")
 
 st.markdown('# Rock fragmentation analysis')
 st.markdown('## semi-Decorous edition')
@@ -33,7 +29,7 @@ with col_units:
 
 original_image, resized_image = load_image(image_selected)
 # st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-st.image(original_image, caption='Original Image', width=5,  use_column_width=2, clamp=False, channels="RGB", output_format="auto")
+# st.image(original_image, caption='Original Image', width=200,  use_column_width=50, clamp=False, channels="RGB", output_format="auto")
 
 area_ball = math.pi*diameter*diameter/4.0
 
@@ -49,34 +45,82 @@ df_masks = df_fixer(df_masks, diameter, beta_df_balls)
 #     st.write(df_balls_data)
 
 
-st.write('Masks Dataframe')
-st.write(df_masks)
+# st.write('Masks Dataframe')
+# st.write(df_masks)
 
-st.write('')
-st.write('Masks Stadistics')
-st.write(df_masks.describe())
+# st.write('')
+# st.write('Masks Stadistics')
+# st.write(df_masks.describe())
 
 plot_fig = show_anns(masks,resized_image, df_masks['area_pixel'].quantile(0.8))
-st.image(plot_fig, caption='Masked Image')
+# st.image(plot_fig, caption='Masked Image')
 
-fig, ax = plt.subplots()
-df_masks['area'].hist(bins=100,ax=ax)
+# fig, ax = plt.subplots()
+# df_masks['area'].hist(bins=100,ax=ax)
+# # x=df_masks['area'].quantile(0.8)
+# ax.axvline(x=df_masks['area'].quantile(0.8), c='r', label='P80')
+# ax.set_ylabel(f'Area {units}^2')
+# ax.set_xlabel('Number of Fragments')
+# plt.legend()
+
+# plt.title('Area Histogram, P80: ' + str(round(df_masks['area'].quantile(0.8),1)) + f'{units}^2')
+# st.pyplot(fig)
+# # st.bar_chart(data=x, *, x=None, y=None, x_label='Number of Fragments', y_label=f'Area {units}^2')
+
+# fig, ax = plt.subplots()
+# df_masks['diameter'].hist(bins=100,ax=ax)
+# ax.axvline(x=df_masks['diameter'].quantile(0.8), c='r', label='P80')
+# ax.set_ylabel(f'Diameter {units}')
+# ax.set_xlabel('Number of Fragments')
+# plt.legend()
+
+# plt.title('Diameter Histogram, P80: ' + str(round(df_masks['diameter'].quantile(0.8),1)) + f'{units}')
+# st.pyplot(fig)
+
+# All outputs
+col1, col2 = st.columns(2)
+with col1:
+  st.expander('Orignal Image')
+  st.image(original_image, caption="Orignal Image",width=500,  use_column_width=100, clamp=False, channels="RGB", output_format="auto")
+with col2:
+  # plot_fig = show_anns(masks,resized_image, df_masks['area_pixel'].quantile(0.8))
+  st.image(plot_fig, caption='Masked Image',width=500,  use_column_width=100, clamp=False, channels="RGB", output_format="auto")
+# csv data
+exp1, exp2 = st.columns(2)
+with exp1:
+    st.write('Masks Dataframe')
+    st.write(df_masks)
+# Describe
+with exp2:
+    st.write('')
+    st.write('Masks Stadistics')
+    st.write(df_masks.describe())
+
+# graph1 
+grp1, grp2 = st.columns(2)
+with grp1:
+    fig, ax = plt.subplots()
+    df_masks['area'].hist(bins=100,ax=ax)
 # x=df_masks['area'].quantile(0.8)
-ax.axvline(x=df_masks['area'].quantile(0.8), c='r', label='P80')
-ax.set_ylabel(f'Area {units}^2')
-ax.set_xlabel('Number of Fragments')
-plt.legend()
+    ax.axvline(x=df_masks['area'].quantile(0.8), c='r', label='P80')
+    ax.set_ylabel(f'Area {units}^2')
+    ax.set_xlabel('Number of Fragments')
+    plt.legend()
+# 
 
-plt.title('Area Histogram, P80: ' + str(round(df_masks['area'].quantile(0.8),1)) + f'{units}^2')
-st.pyplot(fig)
+
+
+# gaph2 
+    plt.title('Area Histogram, P80: ' + str(round(df_masks['area'].quantile(0.8),1)) + f'{units}^2')
+    st.pyplot(fig)
 # st.bar_chart(data=x, *, x=None, y=None, x_label='Number of Fragments', y_label=f'Area {units}^2')
+with grp2:
+    fig, ax = plt.subplots()
+    df_masks['diameter'].hist(bins=100,ax=ax)
+    ax.axvline(x=df_masks['diameter'].quantile(0.8), c='r', label='P80')
+    ax.set_ylabel(f'Diameter {units}')
+    ax.set_xlabel('Number of Fragments')
+    plt.legend()
 
-fig, ax = plt.subplots()
-df_masks['diameter'].hist(bins=100,ax=ax)
-ax.axvline(x=df_masks['diameter'].quantile(0.8), c='r', label='P80')
-ax.set_ylabel(f'Diameter {units}')
-ax.set_xlabel('Number of Fragments')
-plt.legend()
-
-plt.title('Diameter Histogram, P80: ' + str(round(df_masks['diameter'].quantile(0.8),1)) + f'{units}')
-st.pyplot(fig)
+    plt.title('Diameter Histogram, P80: ' + str(round(df_masks['diameter'].quantile(0.8),1)) + f'{units}')
+    st.pyplot(fig)
